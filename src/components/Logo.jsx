8fb394c820be48846adc9021db9ed667c384c8e4
Parth Nonghanvadra <parth.nonghanvadra@spglobal.com>
2025-03-28 13:41:49 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Logo({ width = "100px" }) {
+  return (
+    <img
+      src="https://www.shutterstock.com/image-vector/blog-writing-line-icon-web-600nw-2366232875.jpg"
+      width={width}
+      alt="blog"
+    />
+  );
+}
+
+export default Logo;
