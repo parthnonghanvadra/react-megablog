18af222225a63081ebbb459e82c9c334b29bd1e3
Parth Nonghanvadra <parth.nonghanvadra@spglobal.com>
2025-03-28 14:39:46 +0530

    updated config

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Editor } from "@tinymce/tinymce-react";
 import { Controller } from "react-hook-form";
+import conf from "../conf/conf";
 
 export default function RTE({ name, control, label, defaultValue = "" }) {
   return (
@@ -12,7 +13,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
         control={control}
         render={({ field: { onChange } }) => (
           <Editor
-            apiKey="oyoyrxulgho7cdhp8181ccqsfxrc6yrw5ax9e9jzhqzdkjnp"
+            apiKey={conf.appwriteRTEId}
             initialValue={defaultValue}
             init={{
               initialValue: defaultValue,
